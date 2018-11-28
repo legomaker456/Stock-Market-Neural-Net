c3f7ef07b85060103c47d2e3c33f5d7065196846
Hiren Swaroop <hirenswaroop@Hirens-MacBook-Pro-2.local>
2018-11-28 11:02:29 -0800

    Test

--- neuralNet.py
+++ neuralNet.py
@@ -1,6 +1,6 @@
 import np
 
 class NeuralNet:
-    def __init__(self, x, y):
-        self.input = x
-        self.weight1 = np.random.rand(self.input.shape[1],4)
+	def __init__(self, x, y):
+		self.input = x
+		self.weight1 = np.random.rand(self.input.shape[1],4)
