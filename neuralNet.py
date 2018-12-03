ff3956d6addc0f4ce48e0b41cfa8fdef4599600f
Hiren Swaroop <hirenswaroop@Hirens-MacBook-Pro-2.local>
2018-12-03 09:45:35 -0800

    Sequences

--- neuralNet.py
+++ neuralNet.py
@@ -1,8 +1,8 @@
 import numpy as np 
       
 # Each row is a training example, each column is a feature  [X1, X2, X3]
-x = np.array(([0,0,1], [0,1,1], [1,0,1], [1,1,1]), dtype = float)
-y = np.array(([0], [1], [1], [0]), dtype = float)
+x = np.array(([2,4,6], [1,2,3], [2,7,12], [1,4,7]), dtype = float)
+y = np.array(([2], [1], [5], [3]), dtype = float)
 
 # Define useful functions    
 
@@ -29,8 +29,8 @@ class NeuralNetwork:
 		return self.layer2
 
 	def backprop(self):
-		d_weights2 = np.dot(self.layer1.T, 2 * (self.y - self.output)*sigmoid_derivative(self.output))
-		d_weights1 = np.dot(self.input.T, np.dot(2 * (self.y - self.output)*sigmoid_derivative(self.output), self.weights2.T)*sigmoid_derivative(self.layer1))
+		d_weights2 = np.dot(self.layer1.T, 4 * (self.y - self.output)*sigmoid_derivative(self.output))
+		d_weights1 = np.dot(self.input.T, np.dot(4 * (self.y - self.output)*sigmoid_derivative(self.output), self.weights2.T)*sigmoid_derivative(self.layer1))
 
 		self.weights1 += d_weights1
 		self.weights2 += d_weights2
