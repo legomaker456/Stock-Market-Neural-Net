f300719de063d343a8b8a51b20a5d624e13a306f
Hiren Swaroop <hirenswaroop@Hirens-MacBook-Pro-2.local>
2018-12-05 11:00:13 -0800

    Layer 3

--- neuralNet.py
+++ neuralNet.py
@@ -3,7 +3,7 @@ import numpy as np
 # Each row is a training example, each column is a feature  [X1, X2, X3]
 x = np.array(([.2,.4,.6], [.1,.2,.3], [.1,.5,.9], [.1,.4,.7]), dtype = float)
 y = np.array(([.2], [.1], [.4], [.3]), dtype = float)
-xPredicted = np.array(([.1,.3,.5]), dtype = float)
+xPredicted = np.array(([.1,.7,1.3]), dtype = float)
 
 # Define useful functions    
 
@@ -21,19 +21,23 @@ class NeuralNetwork:
 		self.input = x
 		self.weights1 = np.random.rand(self.input.shape[1], 4) # considering we have 4 nodes in the hidden layer
 		self.weights2 = np.random.rand(4, 1)
+		self.weights3 = np.random.rand(1, 4)
 		self.y = y
 		self.output = np.zeros(y.shape)
 
 	def feedforward(self, x):
 		self.layer1 = sigmoid(np.dot(x, self.weights1))
-		self.layer2 = sigmoid(np.dot(x, self.weights2))
-		return self.layer2
+		self.layer2 = sigmoid(np.dot(self.layer1, self.weights2))
+		self.layer3 = sigmoid(np.dot(self.layer2, self.weights3))
+		return self.layer3
 
 	def backprop(self):
-		d_weights2 = np.dot(self.layer1.T, 2 * (self.y - self.output)*sigmoid_derivative(self.output))
+		d_weights3 = np.dot(self.layer2.T, 2 * (self.y - self.output)*sigmoid_derivative(self.output))
+		d_weights2 = np.dot(self.layer1.T, np.dot(2 * (self.y - self.output)*sigmoid_derivative(self.output), self.weights3.T)*sigmoid_derivative(self.layer2))
 		d_weights1 = np.dot(self.input.T, np.dot(2 * (self.y - self.output)*sigmoid_derivative(self.output), self.weights2.T)*sigmoid_derivative(self.layer1))
 		self.weights1 += d_weights1
 		self.weights2 += d_weights2
+		self.weights3 += d_weights3
 
 	def train(self, x, y):
 		self.output = self.feedforward(x)
@@ -55,7 +59,7 @@ for i in range(1000000): # trains the NN 1,000 times
 		print("Input : \n" + str(x))
 		print("Actual Output: \n" + str(y))
 		print("Predicted Output: \n" + str(neuralNet.feedforward(x)))
-		print("Loss: \n" + str(np.mean(np.square(y - neuralNet.feedforward())))) # mean sum squared loss
+		print("Loss: \n" + str(np.mean(np.square(y - neuralNet.feedforward(x))))) # mean sum squared loss
 		print("\n")
 
 	neuralNet.train(x, y)
