bf83a63d13c204f7717149fa7331cbe41aed5766
Robi Chatterjee <robichatterjee@Robis-MacBook-Pro.local>
2018-12-10 09:40:41 -0800

    updating database file

--- database.py
+++ database.py
@@ -1,1 +1,8 @@
+#Copied from Data science from Scratch by Joel Grus
+class Table:
+    def __init__(self, columns):
+        self.columns = columns
+        self.rows = []
 
+    def __repr__(self):
+        return str(self.columns)
