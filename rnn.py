8dbc8f1d987fad7d365e3dbc49e32282d36a247a
Hiren Swaroop <hirenswaroop@Hirens-MacBook-Pro-2.local>
2018-12-10 09:25:19 -0800

    tanh and sigmoid

--- rnn.py
+++ rnn.py
@@ -1,1 +1,13 @@
-import numpy
+import numpy as np
+
+def sigmoid(t):
+	return 1 / (1 + np.exp(-t))
+
+def sigmoid_derivative(p):
+	return p * (1 - p)
+
+def tanh(t):
+	return np.tanh(t)
+
+def tanh_derivative(p):
+	return 1.0 - np.tanh(p) ** 2
