07560f7a274b0318f75eb8a0f6cbee4c73b5889e
Robi Chatterjee <robichatterjee@Robis-MacBook-Pro.local>
2018-12-12 11:37:23 -0800

    more database code

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ class Table:
     def insert(self, row_values):
         if len(row_values) != len(self.columns):
             raise TypeError("wrong number of elements")
-        row_dict = dict(zip(self.columns, row_values)) #each row is a dict from colun names to values
+        row_dict = dict(zip(self.columns, row_values)) #each row is a dict from column names to values
         self.rows.append(row_dict)
     
     def update(self, updates, predicate):
@@ -26,4 +26,22 @@ class Table:
     def delete(self, predicate=lambda row: True):
         """deletes all rows matching predicate
         if there is no predicate, all rows are deleted"""
+        self.rows = [row for row in self.rows if not(predicate(row))]
+    
+    def select(self, keep_columns = None, additional_columns = None):
+        if keep_columns is None: #if no columns are specified
+            keep_columns = self.columns #returns all columns
+        if additional_columns is None:
+            additional_columns = {}
+
+        #new table for results
+        result_table = Table(keep_columns + additional_columns.keys())
         
+        for row in self.rows:
+            new_row = [row[column] for column in keep_columns]
+            for column_name, calculation in additional_columns.iteritems():
+                new_row.append(calculation(row))
+            result_table.append(calculation(row))
+        
+        return result_table
+
